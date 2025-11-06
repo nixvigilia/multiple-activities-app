d758be6d9549ffd382ebac503e04b0921acfc1dd
Nixoy <john.nicholai.vigilia@gmail.com>
2025-11-07 01:07:01 +0800

    update date type

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -17,8 +17,8 @@ export type Todo = {
   id: string;
   title: string;
   completed: boolean;
-  created_at: string;
-  updated_at: string;
+  created_at: Date;
+  updated_at: Date;
 };
 
 type TodoListProps = {
